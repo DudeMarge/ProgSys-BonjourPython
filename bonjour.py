58948d8cb46e54c3799338662f4de748707a283c
Marjorie Dudemaine <1489515@cstjean.qc.ca>
2020-10-12 13:30:28 -0400

    FIN Réusinage

--- bonjour.py
+++ bonjour.py
@@ -7,9 +7,12 @@ import time
 def main():
     print("Bonjour Python!")
     print("\nJe suis Marjorie Dudemaine")
-    n = 10
-    print(f"et je sais compter jusqu'à {n}: ", end="", flush=True)
-    for i in range(1, n+1):
+    compter_jusqua(10)
+
+
+def compter_jusqua(limite_sup):
+    print(f"et je sais compter jusqu'à {limite_sup}: ", end="", flush=True)
+    for i in range(1, limite_sup + 1):
         time.sleep(0.25)
         print(i, " ", end="", flush=True)
     print()
