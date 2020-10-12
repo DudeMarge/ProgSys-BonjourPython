ee03618e6a7268636c67bdaada4a0cd92b40beca
Marjorie Dudemaine <1489515@cstjean.qc.ca>
2020-10-12 13:23:40 -0400

    Ajout variable n = 10

--- bonjour.py
+++ bonjour.py
@@ -7,8 +7,9 @@ import time
 def main():
     print("Bonjour Python!")
     print("\nJe suis Marjorie Dudemaine")
-    print("et je sais compter jusqu'à 10: ", end="", flush=True)
-    for i in range(1, 11):
+    n = 10
+    print(f"et je sais compter jusqu'à {n}: ", end="", flush=True)
+    for i in range(1, n+1):
         time.sleep(0.25)
         print(i, " ", end="", flush=True)
     print()
