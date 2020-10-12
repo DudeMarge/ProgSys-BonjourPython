414d954cf37f130fe735861b1377e7925b7f723f
Marjorie Dudemaine <1489515@cstjean.qc.ca>
2020-10-12 13:36:26 -0400

    FIN Documentation

--- bonjour.py
+++ bonjour.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
-# Premier script Python
+"""
+Premier script Python
+
+par:Marjorie Dudemaine
+"""
 
 import time
 
 
 def main():
+    """
+    Fonction principale du script
+    """
     print("Bonjour Python!")
     print("\nJe suis Marjorie Dudemaine")
     compter_jusqua(10)
 
 
 def compter_jusqua(limite_sup):
+    """
+    Affiche progressivement une séquence de nombre à partir de 1 en montant
+
+    :param limite_sup: jusqu'à ou compteur
+    """
     print(f"et je sais compter jusqu'à {limite_sup}: ", end="", flush=True)
     for i in range(1, limite_sup + 1):
         time.sleep(0.25)
--- bonjour3.py
+++ bonjour3.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-# 2e script Python
+"""
+2e script Python
+
+par:Marjorie Dudemaine
+"""
 
 import bonjour
 
 
 def main():
+    """
+    Fonction principale du script
+    """
     bonjour.main()
     bonjour.main()
     bonjour.main()
